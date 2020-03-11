aa31ef57e92959f25baba051738488c8074b9a72
Deepakgantayat <deepakgantayat29@gmail.com>
2020-03-10 22:04:30 -0700

    changes

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const cors = require('cors')
 
 const app = express()
 
-const port = 3055
+// const port = 3055
+const port = process.env.PORT || 3055;
+const path = require('path')
+
 const configureDB = require('./config/database')
 const router = require('./config/routes')
 app.use(express.json())
 configureDB()
 app.use(cors())
+
+app.use(express.static(path.join(__dirname,"client/build"))) 
+app.get("*",(req,res) => { 
+    res.sendFile(path.join(__dirname + "/client/build/index.html")) 
+})
 
 app.use('/',router)
 
