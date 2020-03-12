80baedb20ace6f98ed0f4062a754decf9f532077
Deepakgantayat <deepakgantayat29@gmail.com>
2020-03-12 00:09:22 -0700

    changes

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 
 const app = express()
 
 // const port = 3055
 const port = process.env.PORT || 3055;
-const path = require('path')
+
 
 const configureDB = require('./config/database')
 const router = require('./config/routes')
@@ -13,18 +14,16 @@ app.use(express.json())
 configureDB()
 app.use(cors())
 
-app.use(express.static(path.join(__dirname,"client/build"))) 
-app.get("*",(req,res) => { 
-    res.sendFile(path.join(__dirname + "/client/build/index.js")) 
-})
-
 app.use('/',router)
 
 app.get('/', (req,res) => {
     res.send('Welcome to the page')
 })
 
-
+app.use(express.static(path.join(__dirname,"client/build"))) 
+app.get("*",(req,res) => { 
+    res.sendFile(path.join(__dirname + "/client/build/index.html")) 
+}) 
 
 app.listen(port, () => {
     console.log('listening to the port,',port)
