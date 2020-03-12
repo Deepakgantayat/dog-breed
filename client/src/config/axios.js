7b0a1d20ca0ac1669415a100c448d3e04d3eb399
Deepakgantayat <deepakgantayat29@gmail.com>
2020-03-11 22:43:49 -0700

    changes

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,9 +1,11 @@
 import Axios from 'axios'
- 
-//localhost:3025
+
+// const axios = Axios.create({
+//     baseURL: 'http://localhost:3055'
+// })
 
 const axios = Axios.create({
-    baseURL: 'http://localhost:3055'
+    baseURL: '/'
 })
 
 export default axios
