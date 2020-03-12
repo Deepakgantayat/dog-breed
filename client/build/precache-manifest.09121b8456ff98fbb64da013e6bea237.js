42ad4883c3ac602cf580195b834156e349cdb8b6
Deepakgantayat <deepakgantayat29@gmail.com>
2020-03-11 23:05:33 -0700

    changes

---
+++ client/build/precache-manifest.09121b8456ff98fbb64da013e6bea237.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "245b1d02fa1738f78d6ce38eb7127c1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0e01b1749b834634ca0",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "f0e01b1749b834634ca0",
+    "url": "/static/js/2.3801cb78.chunk.js"
+  },
+  {
+    "revision": "46f63f8626516ab37e48a023f0c6fcd4",
+    "url": "/static/js/2.3801cb78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66432163d68bf007a56a",
+    "url": "/static/js/main.9b374787.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
